52c2df82b21d57dc26caec77fe570895ab89c048
Ganesh Chaudhari <69956058+Ganesh-Chaudhari405@users.noreply.github.com>
2022-02-04 21:37:53 +0530

    updated_list

--- singly_linked.cpp
+++ singly_linked.cpp
@@ -166,3 +166,206 @@ int main()
 
 
 }
+
+==================================
+	
+	
+//#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+
+ int data;
+ Node *next;
+
+ Node(int data)
+ {
+  this->data=data;
+  this->next=NULL;
+ }
+
+};
+
+void insertf(Node * &head,int data)
+{ 
+  Node *nnode=new Node(data);
+  // if(!head)
+  // {
+  //   head=nnode;
+  // }
+  // else
+  //{
+    nnode->next=head;
+    head=nnode;
+  //}
+
+
+}
+
+void disply(Node *head)
+{
+  while(head)
+  {
+    cout<<head->data<<" ";
+    head=head->next;
+  }
+
+}
+
+void insertl(Node * &head,int data)
+{
+  Node *nn=new Node(data);
+  if(!head)
+  {
+    head=nn;
+    
+  }
+else
+{
+  Node *temp=head;
+  while(temp->next)
+  {
+    temp=temp->next;
+  }
+  temp->next=nn;
+}
+
+
+}
+void dell(Node * &head)
+{
+  Node *temp=head;
+  Node *tl=NULL;
+  if(!temp->next)
+        delete temp;
+
+  while(temp->next)
+  {
+    tl=temp;
+    temp=temp->next;
+     
+  }
+    tl->next=NULL;
+    cout<<endl<<"Node deleted"<<endl<<temp->data<<endl;
+    delete temp; 
+
+}
+
+void dellf(Node * &head)
+{
+  if(!head)
+    {
+      cout<<"Not possible"<<endl;
+      return;
+    }
+    cout<<"Node is deleted"<<endl<<head->data<<endl;
+    head=head->next;
+
+
+}
+int len(Node *head)
+{
+  int len=0;
+  while(head)
+  {
+    len++;
+    head=head->next;
+  }
+return len;
+
+}
+
+void dellm(Node *&head,int ind)
+{
+  int length=len(head);
+
+  if(ind<1 || ind>length)
+      {
+        cout<<"This is not possible";
+        return;
+      }
+  Node *temp=head;
+  Node *tl=NULL;
+
+    if(ind==1)
+        dellf(head);
+     else if(ind==length)
+         dell(head);
+     else{
+      for(int i=1;i<ind;i++)
+      {
+        tl=temp;
+        temp=temp->next;
+      }
+
+    tl->next=temp->next;
+}
+}
+void insertm(Node * &head,int data,int ind)
+{
+  Node *nn=new Node(data);
+  Node *temp=head;
+   int length=len(head);
+
+  if(ind<1 || ind>length)
+      {
+        cout<<"This is not possible";
+        return;
+      }
+    else if(ind==1)
+    {
+      insertf(head,data);
+    }
+    else
+    {
+
+  for(int i=1;i<ind-1;i++)
+  {
+    temp=temp->next;
+
+  }
+  //temp->next=nn;
+  nn->next=temp->next;
+  temp->next=nn;
+
+}
+}
+
+int main()
+{
+  Node *head=NULL;
+  // insertf(head,10);
+  // insertf(head,20);
+  // insertf(head,30);
+  insertl(head,10);
+  insertl(head,20);
+  insertl(head,30);
+  insertl(head,40);
+  insertl(head,50);
+  //dell(head);
+  //dellf(head);
+   dellm(head,1);
+     insertm(head,678,1);
+  //insertm(head,10);
+
+   disply(head);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
